8978de1b50fb1a163e90387287af0ff56ce2e195
ncorbin003 <71032579+ncorbin003@users.noreply.github.com>
2020-12-22 10:04:19 -0500

    Initial commit

---
+++ app/src/main/java/com/shoppinglistapp/FirstFragment.java
@@ -0,0 +1,65 @@
+package com.shoppinglistapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class FirstFragment extends Fragment {
+
+    ArrayList trackedList = new ArrayList<String>();
+
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        trackedList.add("First item");
+        trackedList.add("Second item");
+        trackedList.add("Third item");
+        trackedList.add("Fourth item");
+
+
+        //Inflate the ListView
+        ListView listView = (ListView) view.findViewById(R.id.list_view);
+        final ListAdapter listAdapter = new ListAdapter(getContext(), trackedList);
+        listView.setAdapter(listAdapter);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                trackedList.add("Adding item to list");
+                listAdapter.updateList(trackedList);
+                System.out.println("text");
+            }
+        });
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/shoppinglistapp/ListAdapter.java
@@ -0,0 +1,58 @@
+package com.shoppinglistapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private ArrayList<String> searchArrayList;
+
+    public ListAdapter(Context context, ArrayList<String> initalList) {
+        searchArrayList = initalList;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public void updateList(ArrayList<String> updatedList){
+        searchArrayList = updatedList;
+        //Triggers the list update
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return searchArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Init a new view that hasn't existed before
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.custom_layout_tv1, parent, false);
+        }
+
+        //Find the Text view layout. Then set the text, color, and size.
+        CheckBox checkBox = convertView.findViewById(R.id.checkBox1);
+
+        checkBox.setText(searchArrayList.get(position));
+        checkBox.setTextColor(Color.BLACK);
+        checkBox.setTextSize(20);
+        return convertView;
+    }
+}
